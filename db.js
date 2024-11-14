4517cc0c0afe976365bc3e9296f98fb799781f75
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-14 14:02:10 -0500

    Creation of sequelize connection

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
-const path = require('path');
 const { Sequelize } = require('sequelize');
+const path = require('path');
 
-// TODO - connect to db via sequelize
+// Create a new Sequelize instance and connect to the database
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: path.join(__dirname, 'database.sqlite'), // Path to the database file
+});
 
-module.exports = {
-    sequelize
-};
+// Export the Sequelize instance for use in models
+module.exports = { sequelize };
